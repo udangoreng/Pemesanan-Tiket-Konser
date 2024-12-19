e04be5dca89f1c609bd394a8587cefca919a332a
hasnatya <hasnatya22@gmail.com>
2024-12-19 14:11:46 +0700

    Add Function, Array, Struct, File

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 #include <iomanip> // Untuk manipulasi output angka
 #include <string>
+#include <fstream>
 using namespace std;
 
-// Konstanta harga tiket
-#define Goldvivpackage 7650000
-#define Cat1 6000000
-#define Silvervivpackage 5150000
-#define Festivala 3500000
-#define Festivalb 2750000
-#define Cat2 3500000
-#define Cat3 2750000
-#define Cat4 1750000
-#define Cat5 1450000
-#define Cat6 1250000
-#define Cat7 950000
+// Struct untuk menyimpan data tiket
+struct Ticket {
+    string name;  // Nama kategori tiket
+    int price;    // Harga tiket
+};
+
+// Konstanta jumlah kategori tiket
+#define NUM_CATEGORIES 11
+
+// Array of struct untuk menyimpan data tiket
+Ticket tickets[NUM_CATEGORIES] = {
+    {"Goldvivpackage", 7650000},
+    {"Cat1", 6000000},
+    {"Silvervivpackage", 5150000},
+    {"Festivala", 3500000},
+    {"Festivalb", 2750000},
+    {"Cat2", 3500000},
+    {"Cat3", 2750000},
+    {"Cat4", 1750000},
+    {"Cat5", 1450000},
+    {"Cat6", 1250000},
+    {"Cat7", 950000}
+};
+
+double totalHarga(int harga, int jumlah){
+    return harga * jumlah;
+}
 
 int main() {
     // Tipe data
@@ -43,44 +59,28 @@ int main() {
     cout << "+++++++++++++++++++++++++++++++++++++++++" << endl;
     cout << "+     Silahkan Pilih Jenis Tiket        +" << endl;
     cout << "+++++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << "1.  Goldvivpackage   : Rp 7.650.000"       << endl;
-    cout << "2.  Cat1             : Rp 6.000.000"       << endl;
-    cout << "3.  Silvervivpackage : Rp 5.150.000"       << endl;
-    cout << "4.  Festivala        : Rp 3.500.000"       << endl;
-    cout << "5.  Festivalb        : Rp 2.750.000"       << endl;
-    cout << "6.  Cat2             : Rp 3.500.000"       << endl;
-    cout << "7.  Cat3             : Rp 2.750.000"       << endl;
-    cout << "8.  Cat4             : Rp 1.750.000"       << endl;
-    cout << "9.  Cat5             : Rp 1.450.000"       << endl;
-    cout << "10. Cat6             : Rp 1.250.000"       << endl;
-    cout << "11. Cat7             : Rp   950.000"       << endl;
-    cout << "++++++++++++++++++++++++++++++++++++++++++"<< endl;
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        cout << i + 1 << ".  " << tickets[i].name << " : Rp " << tickets[i].price << endl;
+    }
+    cout << "++++++++++++++++++++++++++++++++++++++++++" << endl;
 
     // Input pilihan tiket
-    cout << "\nPilih jenis tiket [1-11] : ";
+    cout << "\nPilih jenis tiket [1-" << NUM_CATEGORIES << "] : ";
     cin >> pilihan_tiket;
+
+    // Validasi pilihan tiket
+    if (pilihan_tiket < 1 || pilihan_tiket > NUM_CATEGORIES) {
+    cout << "Pilihan tiket tidak valid!" << endl;
+    return 0;
+    }
 
     // Input jumlah tiket
     cout << "Masukkan jumlah tiket : ";
     cin >> jumlah_tiket;
 
-    // Menentukan nama tiket dan harga
-    switch (pilihan_tiket) {
-        case 1: nama_tiket = "Goldvivpackage"; total_harga = Goldvivpackage * jumlah_tiket; break;
-        case 2: nama_tiket = "Cat1"; total_harga = Cat1 * jumlah_tiket; break;
-        case 3: nama_tiket = "Silvervivpackage"; total_harga = Silvervivpackage * jumlah_tiket; break;
-        case 4: nama_tiket = "Festivala"; total_harga = Festivala * jumlah_tiket; break;
-        case 5: nama_tiket = "Festivalb"; total_harga = Festivalb * jumlah_tiket; break;
-        case 6: nama_tiket = "Cat2"; total_harga = Cat2 * jumlah_tiket; break;
-        case 7: nama_tiket = "Cat3"; total_harga = Cat3 * jumlah_tiket; break;
-        case 8: nama_tiket = "Cat4"; total_harga = Cat4 * jumlah_tiket; break;
-        case 9: nama_tiket = "Cat5"; total_harga = Cat5 * jumlah_tiket; break;
-        case 10: nama_tiket = "Cat6"; total_harga = Cat6 * jumlah_tiket; break;
-        case 11: nama_tiket = "Cat7"; total_harga = Cat7 * jumlah_tiket; break;
-        default:
-            cout << "Pilihan tiket tidak valid!" << endl;
-            return 0;
-    }
+    // Menentukan nama tiket dan total harga
+    nama_tiket = tickets[pilihan_tiket-1].name;
+    total_harga = totalHarga(tickets[pilihan_tiket-1].price, jumlah_tiket);
 
     // Pemilihan waktu
     cout << "+++++++++++++++++++++++++++++++++++++++++" << endl;
@@ -101,14 +101,14 @@ int main() {
         case 2: tanggal_konser = "13 September 2024"; break;
         case 3: tanggal_konser = "14 September 2024"; break;
         default:
-            cout << "Pilihan waktu tidak valid!" << endl;
-            return 0;
+        cout << "Pilihan waktu tidak valid!" << endl;
+        return 0;
     }
 
     // Output total pembayaran
     cout << "\n+++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << "              DETAIL PEMBELIAN         " << endl;
-    cout << "+++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << "              DETAIL PEMBELIAN           " << endl;
+    cout << "+++++++++++++++++++++++++++++++++++++++++" << endl;
     cout << "Nama          : " << nama << endl;
     cout << "No Telepon    : " << no_telepon << endl;
     cout << "Email         : " << email << endl;
@@ -118,7 +118,25 @@ int main() {
     cout << "Jumlah Tiket  : " << jumlah_tiket << endl;
     cout << fixed << setprecision(0); // Menampilkan angka tanpa desimal
     cout << "Total Bayar   : Rp " << total_harga << endl;
-    cout << "+++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << "+++++++++++++++++++++++++++++++++++++++++" << endl;
+
+    ofstream buatStruk("struk.txt");
+    
+    buatStruk << "\n+++++++++++++++++++++++++++++++++++++++" << endl;
+    buatStruk << "              DETAIL PEMBELIAN           " << endl;
+    buatStruk << "+++++++++++++++++++++++++++++++++++++++++" << endl;
+    buatStruk << "Nama          : " << nama << endl;
+    buatStruk << "No Telepon    : " << no_telepon << endl;
+    buatStruk << "Email         : " << email << endl;
+    buatStruk << "Tanggal Beli  : " << tanggal << endl;
+    buatStruk << "Jenis Tiket   : " << nama_tiket << endl;
+    buatStruk << "Tanggal Konser: " << tanggal_konser << endl;
+    buatStruk << "Jumlah Tiket  : " << jumlah_tiket << endl;
+    buatStruk << fixed << setprecision(0); // Menampilkan angka tanpa desimal
+    buatStruk << "Total Bayar   : Rp " << total_harga << endl;
+    buatStruk << "+++++++++++++++++++++++++++++++++++++++++" << endl;
+    
+    buatStruk.close();
 
     return 0;
 }
